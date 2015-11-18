986fbd236bd39faabdc0f4572d40626d1fddc57c
Rodion Artyukhin <artyukhin.rodion@gmail.com>
2015-11-19 01:01:22 +0300

    Added image uploader

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'turbolinks'
 gem 'jade'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'rmagick', '~> 2.15', '>= 2.15.4'
+gem 'carrierwave'
+gem 'mini_magick'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114223224) do
+ActiveRecord::Schema.define(version: 20151118203751) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151114223224) do
     t.integer  "chef_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
 end
