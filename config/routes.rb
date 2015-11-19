40054511a4f7ec46de6a93d121517798f9c87508
Rodion Artyukhin <artyukhin.rodion@gmail.com>
2015-11-20 00:30:07 +0300

    Added likes feature(model, validation and views)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :recipes
+  get '/home', to: 'pages#home'
+  resources :recipes do
+    member do
+      post :like      
+    end
+  end
+
 end
